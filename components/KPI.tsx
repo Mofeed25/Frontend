fa01b175055afbef27ef1883b27aa0434f65ca4d
Mofeed25 <mueedabd92@gmail.com>
2026-06-30 18:27:34 +0300

    Update KPI.tsx

--- components/KPI.tsx
+++ components/KPI.tsx
@@ -1,10 +1,8 @@
 export default function KPI({ title, value }: any) {
     return (
-        <div className="card">
-            <div style={{ color: "#00D4FF", fontSize: 14 }}>{title}</div>
-            <div style={{ fontSize: 24, marginTop: 8 }}>
-                {value}
-            </div>
+        <div>
+            <h3>{title}</h3>
+            <p>{value}</p>
         </div>
     );
 }
