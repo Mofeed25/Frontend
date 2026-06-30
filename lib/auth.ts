602903d52f8638bd19bb2600fb037e20af9118a8
Mofeed25 <mueedabd92@gmail.com>
2026-06-30 17:54:51 +0300

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,4 @@
-export function setToken(token: string) {
-    localStorage.setItem("token", token);
-}
-
 export function getToken() {
+    if (typeof window === "undefined") return null;
     return localStorage.getItem("token");
-}
-
-export function logout() {
-    localStorage.removeItem("token");
 }
