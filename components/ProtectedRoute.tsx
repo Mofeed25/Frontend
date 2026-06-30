0acf3f5ed2174a0cc8cb20ef146b2eff1c54a0e3
Mofeed25 <mueedabd92@gmail.com>
2026-06-30 17:57:03 +0300

    Update ProtectedRoute.tsx

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import { getToken } from "@/lib/auth";
 import { useRouter } from "next/navigation";
+import { getToken } from "../lib/auth";
 
 export default function ProtectedRoute({ children }: any) {
 
     const router = useRouter();
 
     useEffect(() => {
-
         if (!getToken()) {
             router.push("/login");
         }
-
     }, []);
 
     return children;
